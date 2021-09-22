84b9a14887e66e3b2969a802909d1dcbbe141011
AkashPakkurthi <aakuakash3213@gmail.com>
2021-09-22 10:27:56 +0530

    First

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.post("/bmi",function(req,res){
+    const a=Number(req.body.aa);
+    const b=Number(req.body.bb);
+    const c=a/(b*b);
+    res.send("Your BMI is "+c);
+})
+app.listen(process.env.PORT||3000,function(){
+    console.log("kk");
+});
